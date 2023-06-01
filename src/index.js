b8429c501929b28393a8a7777c1b7152835f6205
John Eddy <john.k.eddy.51@gmail.com>
2023-06-01 12:07:18 -0400

    added onload function to render cells and access the svg element on the page. Created class to store cell information

--- src/index.js
+++ src/index.js
@@ -6,25 +6,49 @@
 
 // GLOBALS
 var cells = new Array();
-const svg = document.getElementById("cells");
+var svg = null;
 const height = 700;
 const width = 700;
 const cell_size = 70;
 const svgns = "http://www.w3.org/2000/svg";
 
-// FUNCTIONS
+/*
+ * Class to store element and liveness of each cell.
+*/
+class Cell {
+    constructor(element) {
+        this.element = x;
+        this.alive = true;
+    }
+}
+
+/*
+ * Renders the cells to the svg. Plan to implement on page load.
+*/
 function render_cells() {
     var id = 0;
     for(var y = 0; y < height; y+=cell_size) {
         for(var x = 0; x < width; x+=cell_size) {
             var cell = document.createElementNS(svgns, "rect");
-            cell.setAttribute("x", x);
-            cell.setAttribute("y", y);
-            cell.setAttribute("width", cell_size);
-            cell.setAttribute("height", cell_size);
-            cell.setAttribute("id", toString(id));
+            cell.setAttributeNS(null, "x", x);
+            cell.setAttributeNS(null, "y", y);
+            cell.setAttributeNS(null, "width", cell_size);
+            cell.setAttributeNS(null, "height", cell_size);
+            cell.setAttributeNS(null, "id", toString(id));
+            cell.setAttributeNS(null, "fill", "none");
+            cell.setAttributeNS(null, "stroke", "black");
             svg.appendChild(cell);
-            cells.push(cell);
+            var c = new Cell(cell);
+            cells.push(c);
         }            
     }
 }
+
+/*
+ * On window load, access necessary DOM elements.
+*/
+window.onload = function() {
+    svg = document.getElementById("cells");
+    render_cells();
+}
+
